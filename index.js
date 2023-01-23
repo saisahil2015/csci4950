628d2ba10a52e38b3b1beb552053c2fe16e2e6e6
Sahil Raina <saisahil2015@gmail.com>
2023-01-23 13:18:44 -0600

    Minor change

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function register() {
-  //   names.push(document.getElementById('names').value);
-  //   console.log(names);
   document.getElementById('list').innerHTML +=
     '<li>' + document.getElementById('names').value + '</li>';
-
-  //   alert(names);
 }
